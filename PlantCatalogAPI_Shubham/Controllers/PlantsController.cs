2f7640365dfc75fa388e9453286cfd9c4a10ea86
Shubham Savaliya <shubhamsavaliya2004@gmail.com>
2026-02-23 21:35:16 -0500

    Initial commit - Plant API

---
+++ PlantCatalogAPI_Shubham/Controllers/PlantsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlantCatalogAPI_Shubham.Models;
+
+namespace PlantCatalogAPI_Shubham.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlantsController : ControllerBase
+{
+    private static List<Plant> plants = new List<Plant>
+    {
+        new Plant { Id = 1, Name = "Snake Plant", Type = "Succulent", IsIndoor = true },
+        new Plant { Id = 2, Name = "Rose", Type = "Flower", IsIndoor = false }
+    };
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(plants);
+    }
+
+    [HttpGet("indoor")]
+    public IActionResult GetIndoorPlants()
+    {
+        var indoor = plants.Where(p => p.IsIndoor).ToList();
+        return Ok(indoor);
+    }
+}
---
+++ PlantCatalogAPI_Shubham/Models/Plant.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace PlantCatalogAPI_Shubham.Models;
+
+public class Plant
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public bool IsIndoor { get; set; }
+}
+
